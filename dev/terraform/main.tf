c9a03a0faed66b9d8603bf0791fc4c96f3d69319
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-05 13:13:20 -0600

    Format tf files and remove sparse-checkout

--- dev/terraform/main.tf
+++ dev/terraform/main.tf
@@ -1,13 +1,13 @@
-terraform {    
-  required_providers {    
-    azurerm = {    
-      source = "hashicorp/azurerm"    
-    }    
-  }    
-} 
-   
-provider "azurerm" {    
-  features {}    
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -37,7 +37,7 @@ resource "azurerm_app_service" "app_service" {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
-  
+
   #(Optional)
   app_settings = {
     "SOME_KEY" = "some-value"
