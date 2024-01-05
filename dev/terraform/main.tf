29801d17f8bedb0dbed3161f642f15984c4adcdb
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-05 15:02:18 -0600

    Add vars file and modify var usage

--- dev/terraform/main.tf
+++ dev/terraform/main.tf
@@ -8,6 +8,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  tenant_id       = "${var.azure_tenant_id}"
+  subscription_id = "${var.azure_subscription_id}"
+  client_id       = "${var.azure_client_id}"
+  client_secret   = "${var.azure_client_secret}"
 }
 
 resource "azurerm_resource_group" "resource_group" {
