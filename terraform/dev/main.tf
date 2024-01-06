b991f5a416c6aae02dd52a5e307246f5c32d46a4
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-06 11:19:42 -0600

    Remove ids of azurerm backend

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -10,10 +10,6 @@ terraform {
     storage_account_name = "maar1-web.dev.terraform.tfstate"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-    tenant_id            = var.azure_tenant_id
-    subscription_id      = var.azure_subscription_id
-    client_id            = var.azure_client_id
-    client_secret        = var.azure_client_secret
   }
 }
 
