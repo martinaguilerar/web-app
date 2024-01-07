360855accae4649d2459c8757d9c911a25a9489d
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-06 20:10:00 -0600

    Modify js app

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const express = require("express");
-const sql = require('mssql');
-  
-const app = express();
+const http = require('http');
 
-const config = {
-    authentication: {
-        type: 'default'
-    },
-    options: {
-        encrypt: true
-    }
-}
-  
-app.listen(8080, () => {
-  console.log(`Server is up and running on 8080 ...`);
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get("/", async (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '/index.html'));
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
