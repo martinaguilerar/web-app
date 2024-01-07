956aa0ffe32bd67f163d6db84c8425e097a15fd9
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-07 13:38:12 -0600

    Add resources for kv and app config for showing image

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const { BlobServiceClient } = require('@azure/storage-blob');
+
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const containerName = process.env.sa_container_name;
+const account = process.env.sa_account_name;
+const sas = process.env.sa_sas;
+
+const blobServiceClient = new BlobServiceClient(`https://${account}.blob.core.windows.net${sas}`);
+
+app.get('/', async (req, res) => {
+  const blobName = 'greeting.png';
+
+  try {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobClient = containerClient.getBlobClient(blobName);
+    const response = await blobClient.download();
+    res.setHeader('Content-Type', 'image/png');
+    response.readableStreamBody.pipe(res);
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    res.status(500).send('Internal Server Error');
+  }
 })
 
 app.listen(port, () => {
--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -19,13 +19,62 @@ provider "azurerm" {
   client_secret   = var.azure_client_secret
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "resource_group" {
-  name     = "app-service-rg-dev"
+  name     = "app-service-rg"
   location = "East US"
 }
 
+resource "azurerm_key_vault" "azurerm_keyvault" {
+  name                            = "mywebappmaarkvdev"
+  resource_group_name             = azurerm_resource_group.resource_group.name
+  location                        = azurerm_resource_group.resource_group.location
+  enabled_for_disk_encryption     = true
+  enabled_for_deployment          = true
+  enabled_for_template_deployment = true
+  sku_name                        = "standard"
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "keyvault_secret_container_name" {
+  name         = "containerName"
+  value        = ""
+  key_vault_id = azurerm_key_vault.azurerm_keyvault.id
+  lifecycle {
+    ignore_changes = [value, version]
+  }
+}
+
+resource "azurerm_key_vault_secret" "keyvault_secret_account_name" {
+  name         = "accountName"
+  value        = ""
+  key_vault_id = azurerm_key_vault.azurerm_keyvault.id
+  lifecycle {
+    ignore_changes = [value, version]
+  }
+}
+
+resource "azurerm_key_vault_secret" "keyvault_secret_sas" {
+  name         = "sas"
+  value        = ""
+  key_vault_id = azurerm_key_vault.azurerm_keyvault.id
+  lifecycle {
+    ignore_changes = [value, version]
+  }
+}
+
 resource "azurerm_service_plan" "azurerm_service_plan" {
-  name                = "myappservice-plan-dev"
+  name                = "myappservice-plan"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   os_type             = "Linux"
@@ -33,7 +82,7 @@ resource "azurerm_service_plan" "azurerm_service_plan" {
 }
 
 resource "azurerm_linux_web_app" "azurerm_linux_webapp" {
-  name                = "mywebapp-maar-dev"
+  name                = "mywebapp-maar"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_service_plan.azurerm_service_plan.location
   service_plan_id     = azurerm_service_plan.azurerm_service_plan.id
@@ -44,6 +93,15 @@ resource "azurerm_linux_web_app" "azurerm_linux_webapp" {
 resource "azurerm_linux_web_app_slot" "app_service_slot_dev" {
   name           = "dev"
   app_service_id = azurerm_linux_web_app.azurerm_linux_webapp.id
+  identity {
+      type = "SystemAssigned"
+  }
+
+  app_settings = {
+    "sa_container_name"  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.keyvault_secret_container_name.versionless_id})",
+    "sa_account_name"  = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.keyvault_secret_account_name.versionless_id})",
+    "sa_sas" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.keyvault_secret_sas.versionless_id})"
+  }
 
   site_config {
     application_stack {
@@ -52,8 +110,19 @@ resource "azurerm_linux_web_app_slot" "app_service_slot_dev" {
   }
 }
 
+resource "azurerm_key_vault_access_policy" "azurerm_access_policy" {
+  key_vault_id = azurerm_key_vault.azurerm_keyvault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+
+  object_id = azurerm_linux_web_app_slot.app_service_slot_dev.identity.0.principal_id
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
 resource "azurerm_storage_account" "resources_storage_account" {
-  name                     = "webappmaarsadev"
+  name                     = "appmaarsadev"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
