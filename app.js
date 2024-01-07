790af0cbce7c187b239bf85968381d5ea90a6664
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-06 19:37:31 -0600

    Remove sqlite code and files

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const sql = require('mssql');
 const app = express();
 
 const config = {
-    user: process.env["DB_USER"],
-    password: process.env["DB_PASSWORD"],
-    server: process.env["DB_SERVER"],
-    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: process.env["DB_NAME"],
     authentication: {
         type: 'default'
     },
@@ -22,24 +17,4 @@ app.listen(8080, () => {
 });
 
 app.get("/", async (req, res) => {
-    try {
-      // Create a SQL Server connection pool
-      const pool = await sql.connect(config);
-  
-      // Execute your SQL query
-      const result = await pool.request().query(`SELECT TOP (20) [product_id]
-            ,[product_name]
-            ,[brand_id]
-            ,[category_id]
-            ,[model_year]
-            ,[list_price]
-        FROM [production].[products]`);
-      res.setHeader("Cache-Control", "no-store");
-      res.setHeader("Pragma", "no-cache");
-      // Send the result as JSON
-      res.status(200).json(result.recordset);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Internal Server Error");
-    }
   });
