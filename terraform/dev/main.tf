3c713845968f325ace3def6ac368eb335768d8e3
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-06 19:20:52 -0600

    Add greeting icon resources in tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -24,28 +24,52 @@ resource "azurerm_resource_group" "resource_group" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "azurerm_service_plan" {
   name                = "myappservice-plan-dev"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
 
-  sku {
-    tier = "Standard"
-    size = "S1"
+resource "azurerm_linux_web_app" "azurerm_linux_webapp" {
+  name                = "mywebapp-maar-dev"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_service_plan.azurerm_service_plan.location
+  service_plan_id     = azurerm_service_plan.azurerm_service_plan.id
+
+  site_config {}
+}
+
+resource "azurerm_linux_web_app_slot" "app_service_slot_dev" {
+  name           = "dev"
+  app_service_id = azurerm_linux_web_app.azurerm_linux_webapp.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
   }
 }
 
-resource "azurerm_app_service" "app_service" {
-  name                = "mywebapp-maar-dev"
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+resource "azurerm_storage_account" "resources_storage_account" {
+  name                     = "webappmaarsadev"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
 
-resource "azurerm_app_service_slot" "app_service_slot_dev" {
-  name                = "dev"
-  app_service_name    = azurerm_app_service.app_service.name
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+resource "azurerm_storage_container" "mywebappimages" {
+  name                  = "mywebappimages"
+  storage_account_name  = azurerm_storage_account.resources_storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "greeting" {
+  name                   = "greeting.png"
+  storage_account_name   = azurerm_storage_account.resources_storage_account.name
+  storage_container_name = azurerm_storage_container.mywebappimages.name
+  type                   = "Block"
+  source                 = "../../src/greeting.png"
 }
