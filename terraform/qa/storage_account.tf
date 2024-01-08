635aa2065d5a2258d46826a9eddba96cea599f72
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-07 20:42:18 -0600

    Add files for qa environment

---
+++ terraform/qa/storage_account.tf
@@ -0,0 +1,21 @@
+resource "azurerm_storage_account" "resources_storage_account" {
+  name                     = "appmaarsaqa"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "mywebappimages" {
+  name                  = "mywebappimages"
+  storage_account_name  = azurerm_storage_account.resources_storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "greeting" {
+  name                   = "greeting.png"
+  storage_account_name   = azurerm_storage_account.resources_storage_account.name
+  storage_container_name = azurerm_storage_container.mywebappimages.name
+  type                   = "Block"
+  source                 = "../../src/greeting_qa.png"
+}
