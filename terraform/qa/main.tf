d3e4c985d27472607f1e8c2b90579f667c9a1e72
martinaguilerar <martin.aguileraromero@gmail.com>
2024-01-07 20:55:20 -0600

    Add files for qa environment (#4)

---
+++ terraform/qa/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+
+  backend "azurerm" {
+    key = "qa.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "app-service-rg"
+  location = "East US"
+}
